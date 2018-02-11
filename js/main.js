21070a8320166c08e25bd22120ca6de67c476301
ellunare <ellunare16@gmail.com>
2018-02-11 18:55:15 +0200

    v2.1

--- js/main.js
+++ js/main.js
@@ -24,7 +24,11 @@ const App = (function () {
 					let image = document.createElement('img');
 					image.src = window.URL.createObjectURL(curFiles[i]);
 
-					imgs.appendChild(image);
+					let imgOne = document.createElement('div');
+					imgOne.classList.add('img_one');
+					imgOne.appendChild(image);
+
+					ramka.appendChild(imgOne);
 				}
 			}
 
@@ -34,48 +38,52 @@ const App = (function () {
 
 			function addTag(e) {
 				console.log(e);
+				console.log(e.offsetX, e.offsetY);
 				e.preventDefault();
 				e.stopPropagation();
 
 				if (e.target.parentElement.parentElement.classList.contains('ramka')) {
 					if (!e.srcElement.classList.contains('tag')) {
-						tagX = e.clientX + e.target.parentElement.parentElement.scrollLeft;
-						tagY = e.layerY;
-						console.log(tagX, tagY);
+						tagX = e.offsetX;
+						tagY = e.offsetY;
 						bodyText.innerText = `${tagX} - ${tagY}`;
 
-						drawTag(tagX, tagY);
+						let imgOne = e.srcElement.parentElement;
+						drawTag(tagX, tagY, imgOne);
+					}
+					else {
+						e.srcElement.remove();
 					}
 				}
 
 			}
 
-			function drawTag(x, y) {
+			function drawTag(x, y, container) {
 				const tag = document.createElement('div');
 				tag.classList.add('tag');
 				tag.style.marginLeft = (x - 10) + 'px';
 				tag.style.marginTop = (y - 10) + 'px';
 
-				const min = 0;
-				const max = 9;
+				// const min = 0;
+				// const max = 9;
 
-				function rand() {
-					return (Math.random() * (max - min) + min).toFixed();
-				}
+				// function rand() {
+				// 	return (Math.random() * (max - min) + min).toFixed();
+				// }
 
-				tag.style.backgroundColor =
-					'#' +
-					rand() +
-					rand() +
-					rand() +
-					rand() +
-					rand() +
-					rand()
-					;
+				// tag.style.backgroundColor =
+				// 	'#' +
+				// 	rand() +
+				// 	rand() +
+				// 	rand() +
+				// 	rand() +
+				// 	rand() +
+				// 	rand()
+				// 	;
 
 				tag.innerText = tagId++;
 
-				tagList.insertAdjacentElement('beforeend', tag);
+				container.insertAdjacentElement('afterbegin', tag);
 
 			}
 
